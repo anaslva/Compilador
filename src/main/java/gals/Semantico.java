02def5ff18a220ed288e14af7c138b34c8496163
anaslva <anacarolina.slva@hotmail.com>
2022-12-07 19:20:27 -0300

    implementação dos cases 15 - 35

--- src/main/java/gals/Semantico.java
+++ src/main/java/gals/Semantico.java
@@ -29,23 +29,22 @@ public class Semantico implements Constants
     {
         String tipo1 = "";
         String tipo2 = "";
-        //System.out.println("Ação #"+action+", Token: "+token);
 
         switch (action){
             case 1:
-                verificaTipoNum();
+                verificaTipoNum(token);
                 codigo.append(System.lineSeparator()).append("add");
                 break;
             case 2:
-               verificaTipoNum();
+                verificaTipoNum(token);
                 codigo.append(System.lineSeparator()).append("sub");
                 break;
             case 3:
-                verificaTipoNum();
+                verificaTipoNum(token);
                 codigo.append(System.lineSeparator()).append("mul");
                 break;
             case 4:
-                verificaDivisao();
+                verificaDivisao(token);
                 codigo.append(System.lineSeparator()).append("div");
                 break;
             case 5:
@@ -56,7 +55,6 @@ public class Semantico implements Constants
                         .append(System.lineSeparator())
                         .append("conv.r8");
 
-
                 break;
             case 6:
                 pilhaTipos.push(FLOAT);
@@ -65,11 +63,19 @@ public class Semantico implements Constants
                 break;
             case 7:
                 tipo1 = pilhaTipos.pop();
-                verificaNumValido(tipo1);
+                if(tipo1.equals(INT) || tipo1.equals(FLOAT)){
+                    pilhaTipos.push(tipo1);
+                } else {
+                    throw new SemanticError("tipo(s) incompatível(is) em expressão aritmética", token.getPosition());
+                }
                 break;
             case 8:
                 tipo1 = pilhaTipos.pop();
-                verificaNumValido(tipo1);
+                if(tipo1.equals(INT) || tipo1.equals(FLOAT)){
+                    pilhaTipos.push(tipo1);
+                } else {
+                    throw new SemanticError("tipo(s) incompatível(is) em expressão aritmética", token.getPosition());
+                }
                 codigo.append(System.lineSeparator()).append("ldc.i8 -1");
                 codigo.append(System.lineSeparator()).append("conv.r8");
                 codigo.append(System.lineSeparator()).append("mul");
@@ -78,15 +84,15 @@ public class Semantico implements Constants
                 operdor = token.getLexeme();
                 break;
             case 10:
-                verificaDivisao();
-                if(operdor.equals(">"))
+                verificaDivisao(token);
+                if (operdor.equals(">"))
                     codigo.append(System.lineSeparator()).append("ctg");
                 else if (operdor.equals("<"))
                     codigo.append(System.lineSeparator()).append("ctg");
                 else if (operdor.equals("=="))
                     codigo.append(System.lineSeparator()).append("ceq");
                 else
-                    throw new SemanticError("tipos incompatíveis em expressão relacional");
+                    throw new SemanticError("tipos incompatíveis em expressão relacional", token.getPosition());
                 break;
             case 11:
                 pilhaTipos.push(BOOL);
@@ -97,84 +103,185 @@ public class Semantico implements Constants
                 codigo.append(System.lineSeparator()).append("ldc.i4.0");
                 break;
             case 13:
-                verificaBool();
+                verificaBool(token);
                 codigo.append(System.lineSeparator()).append("ldc.i4.1");
                 codigo.append(System.lineSeparator()).append("xor");
                 break;
             case 14:
                 tipo1 = pilhaTipos.pop();
-                if(tipo1.equals(INT)){
+                if (tipo1.equals(INT)) {
                     codigo.append(System.lineSeparator()).append("conv.i8");
                 }
                 String tipoFormatado = String.format("(\" %S \")", tipo1);
                 codigo.append(System.lineSeparator())
-                        .append("call void [mscorlib]System.Console::Write" )
+                        .append("call void [mscorlib]System.Console::Write")
                         .append(tipoFormatado);
 
                 break;
             case 15:
+                codigo
+                        .append(".assembly extern mscorlib {}").append(System.lineSeparator())
+                        .append(".assembly _codigo_objeto{}").append(System.lineSeparator())
+                        .append(".module   _codigo_objeto.exe").append(System.lineSeparator())
+                        .append(System.lineSeparator())
+                        .append(".class public _UNICA{").append(System.lineSeparator())
+                        .append(".method static public void _principal() {").append(System.lineSeparator())
+                        .append(".entrypoint");
                 break;
             case 16:
+                codigo
+                        .append("ret").append(System.lineSeparator())
+                        .append("}").append(System.lineSeparator())
+                        .append("}");
                 break;
             case 17:
+                codigo
+                        .append(System.lineSeparator()).append("call void ")
+                        .append(System.lineSeparator()).append("[mscorlib]System.Console::Write(string)");
                 break;
             case 18:
+                tipo1 = pilhaTipos.pop();
+                tipo2 = pilhaTipos.pop();
+                verificaBool(token);
+                codigo
+                        .append(System.lineSeparator()).append("and");
                 break;
-
             case 19:
+                tipo1 = pilhaTipos.pop();
+                tipo2 = pilhaTipos.pop();
+                verificaBool(token);
+                codigo
+                        .append(System.lineSeparator()).append("or");
                 break;
             case 20:
+                verificaDivisao(token);
+                codigo
+                        .append(System.lineSeparator()).append("div")
+                        .append(System.lineSeparator()).append("conv.i8");
                 break;
             case 21:
+                pilhaTipos.push(CHAR);
+                codigo
+                        .append(System.lineSeparator());
+                if (token.getLexeme().equals("\\n")) {
+                    codigo
+                            .append("\"\\n\"");
+                    break;
+                } else if (token.getLexeme().equals("\\s")) {
+                    codigo
+                            .append("\" \"");
+                    break;
+                } else if (token.getLexeme().equals("\\t")) {
+                    codigo
+                            .append("\"\\t\"");
+                    break;
+                }
                 break;
             case 22:
-                break;
-            case 23:
+                pilhaTipos.push(CHAR);
+                codigo
+                        .append(System.lineSeparator()).append("ldstr ").append("\"").append(token.getLexeme()).append("\"");
                 break;
             case 24:
                 break;
             case 25:
                 break;
             case 26:
+                codigo
+                        .append(System.lineSeparator()).append(pilhaRotulos.pop()).append(":");
                 break;
             case 27:
                 break;
             case 28:
                 break;
-            case 29:
-                break;
             case 30:
+                if (token.getLexeme().equals("int")) {
+                    tipoVar = INT;
+                } else if (token.getLexeme().equals("real")) {
+                    tipoVar = FLOAT;
+                }
                 break;
             case 31:
+                for (String id : listaId) {
+                    tabelaSimbolos.put(id, tipoVar);
+                    codigo
+                            .append(System.lineSeparator())
+                            .append(".locals(")
+                            .append(tipoVar)
+                            .append(" ")
+                            .append(id)
+                            .append(")");
+
+                }
+                listaId.clear();
                 break;
             case 32:
+                listaId.add(token.getLexeme());
                 break;
             case 33:
+                String id = token.getLexeme();
+                String tipoId = tabelaSimbolos.get(id);
+                pilhaTipos.push(tipoId);
+                codigo
+                        .append(System.lineSeparator())
+                        .append("ldloc ").append(id);
+                if (tipoId.equals(INT)) {
+                    codigo
+                            .append(System.lineSeparator())
+                            .append("conv.r8");
+                }
                 break;
             case 34:
+                String id2 = listaId.pop();
+                String tipoid2 = tabelaSimbolos.get(id2);
+                pilhaTipos.pop();
+                if (tipoid2.equals(INT)) {
+                    codigo
+                            .append(System.lineSeparator()).append("conv.r8");
+                }
+                codigo
+                        .append(System.lineSeparator()).append("stloc ").append(id2);
                 break;
             case 35:
+                for (String id3 : listaId) {
+                    String classe = "";
+                    String tipoid3 = tabelaSimbolos.get(id3);
+                    if (tipoid3.equals(INT)) {
+                        classe = "int64";
+                    } else if (tipoid3.equals(FLOAT)) {
+                        classe = "Double";
+                    }
+                    codigo
+                            .append(System.lineSeparator()).append("call string [mscorlib]System.Console::ReadLine()")
+                            .append(System.lineSeparator()).append("call ")
+                            .append(tipoid3)
+                            .append(" [mscorlib]System.")
+                            .append(classe)
+                            .append("::Parse(string)")
+                            .append(System.lineSeparator()).append("stloc ")
+                            .append(id3);
+                }
+                listaId.clear();
                 break;
-
         }
 
         System.out.println(codigo);
     }
 
-    private void verificaBool() throws SemanticError {
+    private void verificaBool(Token token) throws SemanticError {
         String tipo = pilhaTipos.pop();
         if(tipo.equals(BOOL)){
             pilhaTipos.push(BOOL);
         } else {
-            throw new SemanticError("tipo(s) incompatível(is) em expressão lógica");
+            throw new SemanticError("tipo(s) incompatível(is) em expressão lógica", token.getPosition());
         }
     }
-    private void verificaDivisao() throws SemanticError {
+    private void verificaDivisao(Token token) throws SemanticError {
         String tipo1 = pilhaTipos.pop();
         String tipo2 = pilhaTipos.pop();
 
-        verificaNumValido(tipo1);
-        verificaNumValido(tipo2);
+        verificaNumValido(tipo1, token);
+        verificaNumValido(tipo2, token);
 
         if(tipo1 == tipo2){
             pilhaTipos.push(tipo1);
@@ -183,12 +290,12 @@ public class Semantico implements Constants
         }
     }
 
-    private void verificaTipoNum() throws SemanticError{
+    private void verificaTipoNum(Token token) throws SemanticError{
         String tipo1 = pilhaTipos.pop();
         String tipo2 = pilhaTipos.pop();
 
-        verificaNumValido(tipo1);
-        verificaNumValido(tipo2);
+        verificaNumValido(tipo1, token);
+        verificaNumValido(tipo2, token);
 
         if (tipo1.equals(FLOAT) || tipo2.equals(FLOAT)) {
             pilhaTipos.push(FLOAT);
@@ -197,11 +304,9 @@ public class Semantico implements Constants
         }
     }
 
-    private void verificaNumValido(String tipo) throws SemanticError {
-        if (tipo.equals(INT) || tipo.equals(FLOAT)) {
-            pilhaTipos.push(tipo);
-        } else {
-            throw new SemanticError("tipo(s) incompatível(is) em expressão aritmética");
+    private void verificaNumValido(String tipo, Token token) throws SemanticError {
+        if (!tipo.equals(INT) && !tipo.equals(FLOAT)) {
+            throw new SemanticError("tipo(s) incompatível(is) em expressão aritmética", token.getPosition());
         }
     }
 }
